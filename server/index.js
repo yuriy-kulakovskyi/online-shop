c6ff533e479a9466c338f20c5a562d12a650b18f
yuriy-kulakovskyi <yurakulak42@gmail.com>
2022-11-20 23:11:22 +0200

    MongoDB is connected

--- server/index.js
+++ server/index.js
@@ -1,20 +1,39 @@
 const mongoose = require("mongoose");
 const express = require("express");
+const cors = require("cors");
 const app = express();
+
+const GoodModel = require("./models/good");
+
+app.use(express.json());
+app.use(cors());
 
 mongoose.connect('mongodb+srv://yura:online-shop@shop-cluster.inq9dxh.mongodb.net/?retryWrites=true&w=majority', (err) => {
   console.log(err);
 });
 
-app.use('/', (req, res) => {
-  res.send("Hi");
-});
 app.post('/send', async (req,res)=>{
+
   const {extProduct, codeProduct}=req.body;
+  console.log(extProduct);
+  const good = new GoodModel({name: extProduct, code: codeProduct});
+
   try{
-    res.send(req.body);
+    await good.save();
+    res.send("inserted data");
   } catch (error){
     console.log(error);
   } 
 });
+
+app.get('/read', async (req, res) => {
+  GoodModel.find({}, (err, result) => {
+    if (err) {
+      res.send(err);
+    }
+
+    res.send(result);
+  })
+});
+
 app.listen(3000, () => console.log("Server is listening on port 3000"));
